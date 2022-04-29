56b5758d2bd02e27e1f0922e254d1f81f149f8c6
iakkivr <smolskyi.vitalii@gmail.com>
2022-04-29 12:07:46 +0300

    refactor migrate tests

--- db/migrate/20220428091425_create_tests.rb
+++ db/migrate/20220428091425_create_tests.rb
@@ -4,7 +4,7 @@ class CreateTests < ActiveRecord::Migration[6.0]
       t.string :title, limit: 50, null: false
       t.integer :level, default: 0, null: false
       t.references :category, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :user }
       t.timestamps
     end
   end
