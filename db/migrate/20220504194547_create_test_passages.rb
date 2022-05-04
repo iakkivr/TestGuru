1698aef5a1724fe48453108ead8ba0ada9c48ae9
iakkivr <smolskyi.vitalii@gmail.com>
2022-05-04 22:49:06 +0300

    refactor rename migrate  result => test passage

--- db/migrate/20220504194547_create_test_passages.rb
+++ db/migrate/20220504194547_create_test_passages.rb
@@ -1,6 +1,6 @@
-class CreateResults < ActiveRecord::Migration[6.0]
+class CreateTestPassages < ActiveRecord::Migration[6.0]
   def change
-    create_table :results do |t|
+    create_table :test_passages do |t|
       t.references :user, null: false, foreign_key: true
       t.references :test, null: false, foreign_key: true
 
