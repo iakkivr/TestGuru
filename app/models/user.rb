9e051a719dd44ad7318a3a5ec854e9be7bcb5280
iakkivr <smolskyi.vitalii@gmail.com>
2022-05-07 17:55:28 +0300

    refactor bug in associated User

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@
 #
 class User < ApplicationRecord
   has_many :created_tests, class_name: "Test", foreign_key: "author_id", dependent: :destroy
-  has_many :tests_passages, dependent: :destroy
-  has_many :tests, through: :tests_users
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages
 
   def tests_by_user(level)
     tests.where(level: level)
