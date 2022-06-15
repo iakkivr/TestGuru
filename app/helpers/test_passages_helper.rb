1340f1967d6014a44390d24e7552c957cbf9581c
iakkivr <smolskyi.vitalii@gmail.com>
2022-06-16 00:00:26 +0300

    add helper TestPassage

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,7 @@
 module TestPassagesHelper
+
+  def serial_number_question(test)
+    "Вопрос #{test.questions.map(&:id).index(@test_passage.current_question.id) + 1} из #{test.questions.count} "
+  end
+
 end
