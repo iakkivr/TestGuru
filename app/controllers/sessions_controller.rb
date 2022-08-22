96d5dbfac182e910cdc99c71b85868cf19cbaf45
iakkivr <smolskyi.vitalii@gmail.com>
2022-08-22 20:15:47 +0300

    refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,12 @@
 class SessionsController < ApplicationController
 
-  include SessionsHelper
-
-  def new
-
-  end
+  def new;end
 
   def create
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       log_in user
-      redirect_to cookies[:requested_url] ||= tests_path
+      redirect_to cookies.delete(:requested_url) || tests_path
     else
       flash.now[:alert] = 'Are you a Guru? Verify your Email and Password please'
       render :new
@@ -19,7 +15,16 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out
-    cookies[:requested_url] = tests_path
     redirect_to root_path
   end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
 end
